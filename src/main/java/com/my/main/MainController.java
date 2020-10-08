2c5e29924d1558f60303162e8198c5654fd3b1ef
KIMYONGMIN <KIMYONGMIN@YONGMIN-15ZD960>
2020-10-08 22:45:16 +0900

    spring setting

--- src/main/java/com/my/main/MainController.java
+++ src/main/java/com/my/main/MainController.java
@@ -1,14 +1,26 @@
 package com.my.main;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
 
 	@RequestMapping("/")
-	public void main (){
+	public String main(){
 		System.out.println("123123");
+
+		return "index";
+	}
+	@RequestMapping("/test")
+	@ResponseBody public Map<String, Object> test(){
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("zzzz", "Cccc");
+		return map;
 	}
 }
